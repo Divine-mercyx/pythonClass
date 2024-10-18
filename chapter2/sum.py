52244481364605b94417204d0bfdfea2a5e4cc45
Divine Obinali <divinewilson766@gmail.com>
2024-10-18 09:14:35 +0100

    Updated sum.py on git hub

--- chapter2/sum.py
+++ chapter2/sum.py
@@ -3,18 +3,9 @@ number_2 = int(input("enter second number: "))
 number_3 = int(input("enter third number: "))
 
 sum = number_1 + number_2 + number_3
-average = (number_1 + number_2 + number_3) / 3
+average = sum / 3
 product = number_1 * number_2 * number_3
-smallest_number = number_1
-largest = number_1
+smallest = min(number_1, number_2, number_3)
+largest = max(number_1, number_2, number_3)
 
-if number_2 < smallest_number:
-    smallest_number = number_2
-elif number_3 < smallest_number:
-    smallest_number = number_3
-elif number_2 > largest:
-    largest = number_2
-elif number_3 > smallest_number:
-    largest = number_3
-
-print(f"sum: {sum}\naverage: {average}\nproduct: {product}\nsmallest number: {smallest_number}\nlargest number: {largest}")
+print(f"sum: {sum}\naverage: {average}\nproduct: {product}\nsmallest number: {smallest}\nlargest number: {largest}")
