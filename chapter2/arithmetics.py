9d61c3ad1538f15dc9ada4d5e20eb402fae45f73
Divine Obinali <divinewilson766@gmail.com>
2024-10-19 08:21:23 +0100

    added more to it

---
+++ chapter2/arithmetics.py
@@ -0,0 +1,14 @@
+expressionA = 27.5 + 2
+expressionB = 27.5 - 2
+expressionC = 27.5 * 2
+expressionD = 27.5 / 2
+expressionE = 27.5 // 2
+expressionF = 27.5 ** 2
+
+
+print(expressionA)
+print(expressionB)
+print(expressionC)
+print(expressionD)
+print(expressionE)
+print(expressionF)
---
+++ chapter2/diameter.py
@@ -0,0 +1,7 @@
+radius = int(input("enter the radius: "))
+
+diameter = 2 * radius
+circumference = 2 * 3.14159 * radius
+area = 3.13159 * radius * radius
+
+print(f"diameter: {diameter}\ncircumference: {circumference}\narea: {area}")
