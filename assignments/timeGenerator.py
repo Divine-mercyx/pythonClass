dcdae97a6186bf243b2e7cdc8c8c7b2a077e18f5
Divine Obinali <divinewilson766@gmail.com>
2024-10-27 21:50:51 +0100

    adding the last one

---
+++ assignments/timeGenerator.py
@@ -0,0 +1,82 @@
+count = 0
+count2 = 0
+count3 = 0
+count4 = 0
+count5 = 0
+count6 = 0
+count7 = 0
+count8 = 0
+count9 = 0
+
+number = 1
+number2 = 2
+number3 = 3
+number4 = 4
+number5 = 5
+number6 = 6
+number7 = 7
+number8 = 8
+number9 = 9
+
+while count <= 9:
+    print(f"{number} x {count} = {number * count}\n")
+    count = count + 1
+
+print()
+
+
+while count2 <= 9:
+    print(f"{number2} x {count2} = {number2 * count2}\n")
+    count2 = count2 + 1
+
+print()
+
+
+while count3 <= 9:
+    print(f"{number3} x {count3} = {number3 * count3}\n")
+    count3 = count3 + 1
+
+print()
+
+
+while count4 <= 9:
+    print(f"{number4} x {count4} = {number4 * count4}\n")
+    count4 = count4 + 1
+
+print()
+
+
+while count5 <= 9:
+    print(f"{number5} x {count5} = {number5 * count5}\n")
+    count5 = count5 + 1
+
+print()
+
+
+while count6 <= 9:
+    print(f"{number6} x {count6} = {number6 * count6}\n")
+    count6 = count6 + 1
+
+print()
+
+
+while count7 <= 9:
+    print(f"{number7} x {count7} = {number7 * count7}\n")
+    count7 = count7 + 1
+
+print()
+
+
+while count8 <= 9:
+    print(f"{number8} x {count8} = {number8 * count8}\n")
+    count8 = count8 + 1
+
+print()
+
+
+while count9 <= 9:
+    print(f"{number9} x {count9} = {number9 * count9}\n")
+    count9 = count9 + 1
+
+
+print()
