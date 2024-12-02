595aef6a90b9d6036e550c7ac190e02246d3d5c2
Divine Obinali <divinewilson766@gmail.com>
2024-12-02 14:48:51 +0100

    updated sum_of_all_digits

--- phaseGateOne/sum_of_all_digits.py
+++ phaseGateOne/sum_of_all_digits.py
@@ -2,6 +2,18 @@
 Psuedocode
 -----------------------------------------------
 prompt the user to enter a number between 0 and 1000
+collect the response
+store the response as number
+if number is less than zero
+return invalid input, number below zero
+create a variable total
+assign zero to total
+while number is greater than zero
+number % 10
+store the result as remainder
+plus remainder to total
+divide number by 10
+and the result should be assigned to number
 """
 def sum_all_digits():
     number = int(input("enter a number between 0 and 1000 to get the sum of all digits: "))
